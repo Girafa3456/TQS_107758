cab2a92a2d464f962db7a41cb79930e7ac264a76
Girafa3456 <goncalo.monteiro@ua.pt>
2025-02-18 18:39:44 +0000

    readme done

--- Lab1/tqsstack/src/test/java/com/example/MyStackTest.java
+++ Lab1/tqsstack/src/test/java/com/example/MyStackTest.java
@@ -2,7 +2,6 @@ package com.example;
 
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
