af7956fe94cea09d2972b01cfc914b4371403fa1
goncalo <goncalo.monteiro@ua.pt>
2025-04-09 13:36:05 +0100

    fixed import

--- HW1-107758/src/main/java/fullstack/homeWork/controller/BookingController.java
+++ HW1-107758/src/main/java/fullstack/homeWork/controller/BookingController.java
@@ -1,7 +1,4 @@
 package fullstack.homeWork.controller;
-
-import javax.print.DocFlavor.STRING;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
